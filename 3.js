08fc3072c8e601cc9018eb0a000d50b496c1c893
Thiago Ferreira Teixeira <thferreira755@gmail.com>
2026-06-11 21:08:49 -0300

    Questão 3

---
+++ 3.js
@@ -0,0 +1,29 @@
+const readline = require('readline/promises');
+const {stdin: input, stdout:output} = require('process');
+
+async function iniciar() {
+    const rl = readline.createInterface({input, output});
+    let resp = "s";
+    while(resp === "s"){
+        let num = Number(await rl.question('entre com o valor a ser calculado: '));
+        factorial(num);
+        resp = String(await rl.question('Deseja calcular outro número? Digite [S] ou [N]: '));
+        resp = resp.toLocaleLowerCase();
+        while(resp !== "s" && resp !== "n"){
+            console.log("Responda com [S] ou [N]");
+            resp = String(await rl.question('Deseja calcular outro número? Digite [S] ou [N]: ')).toLocaleLowerCase();
+        }
+    }
+
+    rl.close();
+}
+
+function factorial(num){
+    let res = 1;
+    for(let i = num; i > 0; i--){
+        res = res * i;     
+    }
+    console.log(`${num}! = ${res}`);
+
+}
+iniciar();
